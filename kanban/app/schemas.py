b1b02a83409a22fa20cd7552a276883472b42766
Rami Sfari <rami2sfari@gmail.com>
2020-01-24 14:11:59 +0100

    Fix task and analytics schema 'id issue'

--- kanban/app/schemas.py
+++ kanban/app/schemas.py
@@ -4,7 +4,7 @@ from marshmallow.validate import Length
 
 
 class TaskSchema(ma.ModelSchema):
-    _id = ma.Int(attribute="id", dump_only=True)
+    _id = ma.Int(data_key="id", dump_only=True)
     title = ma.String(max_length=150, required=True, validate=Length(max=150, min=1))
     description = ma.String(max_length=500, required=True, validate=Length(max=500, min=1))
     status = ma.String(max_length=200, required=True, validate=Length(max=200, min=1))
@@ -15,7 +15,7 @@ class TaskSchema(ma.ModelSchema):
 
 class AnalyticsSchema(ma.ModelSchema):
 
-    _id = ma.Int(attribute="id", dump_only=True)
+    _id = ma.Int(data_key="id", dump_only=True)
     event = ma.String(max_length=1000, required=False, validate=Length(max=1000))
     data = ma.Dict(keys=ma.Str(), values=ma.Str())
 
